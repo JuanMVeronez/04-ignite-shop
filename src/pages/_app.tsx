9409c458be1b8231354203ed904c8d880f3070c4
juan.monteirov@gmail.com <juan.monteirov@gmail.com>
2022-10-16 18:58:25 -0300

    creates header styled component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+import logoSvg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
+
 globalStyles();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <Container>
+      <Header>
+        <Image src={logoSvg} alt="ignite shop logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
