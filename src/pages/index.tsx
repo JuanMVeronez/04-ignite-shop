f6371afb10833b7529c203fe744eb16bad7860de
juan.monteirov@gmail.com <juan.monteirov@gmail.com>
2022-10-17 06:31:31 -0300

    starts products page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,46 @@
+import Image from "next/future/image";
 import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  padding: '0.5rem 1rem',
-  border: 0,
-  span: {
-    fontWeight: '700',
-    color: 'white'
-  },
-  '&:hover': {
-    transition: '0.4s filter',
-    filter: 'brightness(0.8)'
-  }
-});
+import shirt0 from '../assets/products/Shirt-0.png'
+import shirt1 from '../assets/products/Shirt-1.png'
+import shirt2 from '../assets/products/Shirt-2.png'
+import shirt3 from '../assets/products/Shirt-3.png'
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Button><span>Rocketseat</span>submit</Button>
-    </>
-    
+    <HomeContainer>
+      <Product>
+        <Image src={shirt0} alt="Camiseta 0" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 0</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+      
+      <Product>
+        <Image src={shirt1} alt="Camiseta 1" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} alt="Camiseta 2" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt3} alt="Camiseta 3" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
