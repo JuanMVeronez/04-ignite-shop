7ef9343dd63be17856573cf8dceb04c8c788f49f
Juan Veronez <juan.monteiro@venturus.org.br>
2022-10-18 08:16:45 -0300

    creates products carousel
    - adds keen-slider as dependency to create de carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
 import Image from "next/future/image";
-import { styled } from "../styles";
 import { HomeContainer, Product } from "../styles/pages/home";
+import { useKeenSlider } from 'keen-slider/react';
 
 import shirt0 from '../assets/products/Shirt-0.png'
 import shirt1 from '../assets/products/Shirt-1.png'
 import shirt2 from '../assets/products/Shirt-2.png'
 import shirt3 from '../assets/products/Shirt-3.png'
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2.5,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt0} alt="Camiseta 0" width={520} height={480} />
         <footer>
           <strong>Camiseta 0</strong>
@@ -18,7 +27,7 @@ export default function Home() {
         </footer>
       </Product>
       
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt1} alt="Camiseta 1" width={520} height={480} />
         <footer>
           <strong>Camiseta 1</strong>
@@ -26,7 +35,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} alt="Camiseta 2" width={520} height={480} />
         <footer>
           <strong>Camiseta 2</strong>
@@ -34,7 +43,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt3} alt="Camiseta 3" width={520} height={480} />
         <footer>
           <strong>Camiseta 3</strong>
